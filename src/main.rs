292ad2d55efe5dae2174fd96ba8411995162488a
jpanahon <36034467+jpanahon@users.noreply.github.com>
2025-10-29 12:49:47 -0400

    feat: Added queue list and volume controls

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ struct Playlist {
 
 fn load_songs(main_dir: String) -> Vec<Song<std::io::BufReader<std::fs::File>>> {
     let mut songs: Vec<Song<std::io::BufReader<std::fs::File>>> = Vec::new();
+
     for entry in std::fs::read_dir(main_dir).expect("Music folder found!") {
         let entry = entry.expect("Entries found!");
         let path = entry.path();
@@ -55,6 +56,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let play_symbols = ["▶", "⏸"];
     let mut play_state = play_symbols[0];
 
+    let mut player_vol: u32 = 100;
+
     let mut playlist: Playlist = Playlist {
         name: "My Playlist".to_string(),
         songs: load_songs("/home/morose/Music/My Playlist/".to_string()),
@@ -64,70 +67,107 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let _ = eframe::run_simple_native("Sanctum Player", options, move |ctx, _frame| {
         egui::TopBottomPanel::bottom("play_bar").show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                let play_button = egui::Button::new(play_state)
-                    .min_size(egui::Vec2::new(40.0, 40.0))
-                    .corner_radius(100)
-                    .frame(true);
+            let play_button = egui::Button::new(
+                egui::RichText::new(play_state).font(egui::FontId::proportional(18.0)),
+            )
+            .min_size(egui::Vec2::new(40.0, 40.0))
+            .corner_radius(100)
+            .frame(true);
 
-                let prev_button = egui::Button::new("⏮︎")
-                    .min_size(egui::Vec2::new(40.0, 40.0))
-                    .frame(false);
-
-                let skip_button = egui::Button::new("⏭︎")
+            let prev_button =
+                egui::Button::new(egui::RichText::new("⏪").font(egui::FontId::proportional(18.0)))
                     .min_size(egui::Vec2::new(40.0, 40.0))
                     .frame(false);
 
-                let shufl_button = egui::Button::new("🔀")
+            let skip_button =
+                egui::Button::new(egui::RichText::new("⏩").font(egui::FontId::proportional(18.0)))
                     .min_size(egui::Vec2::new(40.0, 40.0))
                     .frame(false);
 
-                let repeat_button = egui::Button::new("🔁")
+            let shufl_button =
+                egui::Button::new(egui::RichText::new("🔀").font(egui::FontId::proportional(18.0)))
                     .min_size(egui::Vec2::new(40.0, 40.0))
                     .frame(false);
 
-                if ui.add(prev_button).clicked() {
-                    println!("Previously");
-                }
+            let repeat_button =
+                egui::Button::new(egui::RichText::new("🔁").font(egui::FontId::proportional(18.0)))
+                    .min_size(egui::Vec2::new(40.0, 40.0))
+                    .frame(false);
 
-                if ui.add(play_button).clicked() {
-                    if sink.is_paused() {
-                        sink.play();
-                        play_state = play_symbols[1];
+            ui.columns(6, |columns| {
+                columns[0].horizontal_centered(|ui| {
+                    if song_queue.len() > 0 {
+                        ui.heading(&song_queue[0]);
                     } else {
-                        sink.pause();
-                        play_state = play_symbols[0];
+                        ui.heading("No song playing!");
                     }
-                }
+                });
 
-                if ui.add(skip_button).clicked() {
-                    sink.skip_one();
-                }
+                columns[3].vertical_centered(|ui| {
+                    ui.horizontal(|ui| {
+                        if ui.add(prev_button).clicked() {
+                            println!("Previously");
+                        }
 
-                if ui.add(shufl_button).clicked() {
-                    println!("Shuffle");
-                }
+                        if ui.add(play_button).clicked() {
+                            if sink.is_paused() {
+                                sink.play();
+                                play_state = play_symbols[1];
+                            } else {
+                                sink.pause();
+                                play_state = play_symbols[0];
+                            }
+                        }
 
-                if ui.add(repeat_button).clicked() {
-                    println!("Repeat");
-                }
-            })
+                        if ui.add(skip_button).clicked() {
+                            sink.skip_one();
+                            song_queue.remove(0);
+                        }
+
+                        if ui.add(shufl_button).clicked() {
+                            println!("Shuffle");
+                        }
+
+                        if ui.add(repeat_button).clicked() {
+                            println!("Repeat");
+                        }
+                    });
+                });
+
+                columns[5].horizontal_centered(|ui| {
+                    ui.label("🔈");
+                    ui.add(egui::Slider::new(&mut player_vol, 0..=100));
+                    if player_vol < 100 {
+                        let new_vol: f32 = (player_vol as f32) / (100 as f32);
+                        sink.set_volume(new_vol);
+                    }
+                });
+            });
         }); // egui::TopBottomPanel
 
         egui::SidePanel::left("playlists").show(ctx, |ui| {
-            ui.heading("Playlists");
-            ui.label(&playlist.name);
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                ui.heading("Playlists");
+                ui.label(&playlist.name);
 
-            ui.heading("Song Queue");
-            for song in &song_queue {
-                ui.label(song);
-            }
+                ui.heading(format!("Song Queue {}", sink.len()));
+
+                if song_queue.len() > sink.len() {
+                    song_queue.remove(0);
+                }
+
+                for song in &song_queue {
+                    ui.label(song);
+                    ui.separator();
+                }
+            });
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             egui::ScrollArea::vertical().show(ui, |ui| {
                 ui.vertical_centered(|ui| {
-                    for song in &mut playlist.songs {
+                    for list_index in 0..playlist.songs.len() {
+                        let song = &mut playlist.songs[list_index];
                         let song_entry = egui::Label::new(
                             egui::RichText::new(format!(
                                 "{}\n{}\n{}\n",
@@ -137,13 +177,22 @@ fn main() -> Result<(), Box<dyn Error>> {
                         );
 
                         if ui.add(song_entry).clicked() {
-                            if let Some(decoder) = song.decoder.take() {
-                                if !sink.is_paused() {
+                            if sink.empty() {
+                                for queue_index in list_index..playlist.songs.len() {
+                                    let next_song = &mut playlist.songs[queue_index];
+                                    if let Some(decoder) = next_song.decoder.take() {
+                                        sink.append(decoder);
+                                        song_queue.push(format!(
+                                            "{} - {}",
+                                            next_song.title, next_song.artist
+                                        ));
+                                    }
+                                    play_state = play_symbols[1];
+                                }
+                            } else {
+                                if let Some(decoder) = song.decoder.take() {
                                     sink.append(decoder);
                                     song_queue.push(format!("{} - {}", song.title, song.artist));
-                                } else {
-                                    sink.append(decoder);
-                                    play_state = play_symbols[1];
                                 }
                             }
                         }
